cda12c6dfadfd203af24a74cbf218e61f31c008e
brandovlee <165731814+brandovlee@users.noreply.github.com>
2025-02-20 16:12:08 -0800

    Updated DML.sql to include destination activity intersection table. Added UPDATEs for FKs in each table. Allow user to update Travel_Plans with a NULL destination_id.

--- sql/DML.sql
+++ sql/DML.sql
@@ -9,8 +9,11 @@ VALUES (%%first_name%%, %%last_name%%, %%email%%, %%phone_number%%, %%creation_d
 
 UPDATE `Users`
 SET 
+    `first_name` = %%first_name%%,
+    `last_name` = %%last_name%%,
     `email` = %%email%%,
     `phone_number` = %%phone_number%%
+    `creation_date` = %%creation_date%%
 WHERE `user_id` = %%user_id%%;
 
 DELETE FROM `Users`
@@ -19,17 +22,24 @@ WHERE `user_id` = %%user_id%%;
 -- Travel Plan Queries
 SELECT *
 FROM `Travel_Plans`
-WHERE `user_id` = %%user_id%%;
+WHERE `plan_id` = %%plan_id%%;
 
 INSERT INTO `Travel_Plans` (`user_id`, `destination_id`, `start_date`, `end_date`, `budget`, `status`)
 VALUES (%%user_id%%, %%destination_id%%, %%start_date%%, %%end_date%%, %%budget%%, %%status%%);
 
 UPDATE `Travel_Plans`
 SET 
+    `user_id` = %%user_id%%,
+    `destination_id` = %%destination_id%%,
     `start_date` = %%start_date%%,
     `end_date` = %%end_date%%,
     `budget` = %%budget%%,
     `status` = %%status%%
+WHERE `plan_id` = %%plan_id%%;
+
+UPDATE `Travel_Plans`
+SET `destination_id` = NULL -- Maintain travel plan even if destination is deleted
+WHERE `destination_id` = %%destination_id%%;
 WHERE `plan_id` = %%plan_id%%;
 
 DELETE FROM `Travel_Plans`
@@ -52,6 +62,18 @@ WHERE `destination_id` = %%destination_id%%;
 
 DELETE FROM `Destinations`
 WHERE `destination_id` = %%destination_id%%;
+
+-- Destination Activity Queries
+SELECT da.destination_activity_id, a.name, a.type
+FROM `Destinations_Activities` AS da
+JOIN `Activities` AS a ON da.activity_id = a.activity_id
+WHERE da.destination_id = %%destination_id%%;
+
+INSERT INTO `Destinations_Activities` (`destination_id`, `activity_id`)
+VALUES (%%destination_id%%, %%activity_id%%);
+
+DELETE FROM `Destinations_Activities`
+WHERE `destination_activity_id` = %%destination_activity_id%%;
 
 -- Activity Queries
 SELECT *
@@ -80,6 +102,7 @@ VALUES (%%destination_id%%, %%name%%, %%cost_per_night%%, %%rating%%);
 
 UPDATE `Hotels`
 SET 
+    `destination_id` = %%destination_id%%,
     `name` = %%name%%,
     `cost_per_night` = %%cost_per_night%%,
     `rating` = %%rating%%
